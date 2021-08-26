57b0ac3e772ed17b6a4f16fb5e65732079d8fd10
Maarten van Gompel <proycon@anaproy.nl>
2021-08-26 23:35:28 +0200

    slightly smaller default font

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 #ifndef config_def_h_INCLUDED
 #define config_def_h_INCLUDED
 
-static const char *default_font = "Monospace 15";
+static const char *default_font = "Monospace 14";
 
 struct clr_scheme scheme = {
 	/* colors */
