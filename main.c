0b158b89d36c6352180d30b8186764b59777c435
Frank Oltmanns <frank@oltmanns.dev>
2023-09-17 09:09:58 +0200

    mk: Activate motion keys per command line switch

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 
 #include "keyboard.h"
 #include "config.h"
+#include "motion_key.h"
 
 /* lazy die macro */
 #define die(...)                                                               \
@@ -136,7 +137,7 @@ static void flip_landscape();
 static void show();
 
 /* event handlers */
-static const struct wl_pointer_listener pointer_listener = {
+static struct wl_pointer_listener pointer_listener = {
     .enter = wl_pointer_enter,
     .leave = wl_pointer_leave,
     .motion = wl_pointer_motion,
@@ -144,7 +145,7 @@ static const struct wl_pointer_listener pointer_listener = {
     .axis = wl_pointer_axis,
 };
 
-static const struct wl_touch_listener touch_listener = {
+static struct wl_touch_listener touch_listener = {
     .down = wl_touch_down,
     .up = wl_touch_up,
     .motion = wl_touch_motion,
@@ -1002,6 +1003,15 @@ main(int argc, char **argv)
                    (!strcmp(argv[i], "--list-layers"))) {
             list_layers();
             exit(0);
+        } else if ((!strcmp(argv[i], "-motion-keys")) ||
+                   (!strcmp(argv[i], "--motion-keys"))) {
+            touch_listener.up = wl_touch_up_mk;
+            touch_listener.down = wl_touch_down_mk;
+            touch_listener.motion = wl_touch_motion_mk;
+
+            pointer_listener.button = wl_pointer_button_mk;
+            pointer_listener.leave = wl_pointer_leave_mk;
+            pointer_listener.motion = wl_pointer_motion_mk;
         } else {
             fprintf(stderr, "Invalid argument: %s\n", argv[i]);
             usage(argv[0]);
