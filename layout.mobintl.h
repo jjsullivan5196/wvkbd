e0ecf388536f6157192f3d425987754a7d981afa
Viacheslav Gagara <viacheslavg@gmail.com>
2025-04-09 22:02:37 +0300

    add missing cyrillic letter from ukrainian alphabet
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

--- layout.mobintl.h
+++ layout.mobintl.h
@@ -1626,6 +1626,7 @@ static struct key keys_compose_cyr_tse[] = {
 
 static struct key keys_compose_cyr_g[] = {
   {"ѓ", "Ѓ", 1.0, Copy, 0x0453, 0, 0x0403},
+  {"ґ", "Ґ", 1.0, Copy, 0x0491, 0, 0x0490},
   {"", "", 9.0, Pad},
   {"", "", 0.0, EndRow},
   {"", "", 0.0, EndRow},
