ad4809739aead84bebbc59ac2893c8dccf23d8af
Jun Aruga <jun.aruga@gmail.com>
2025-08-09 18:51:20 +0200

    Add deskintl (desktop international) layout set.
    Add a layout set aimed at desktop, laptop, and tablet devices with a larger
    touchscreen. The layout is the US-International English, adding a compose key.
    https://en.wikipedia.org/wiki/QWERTY#US-International
    
    This layout set was created based on the layout set by the user nine7nine
    (Jordan Johnston) <https://github.com/nine7nine>.
    
    Note I copied the keymap.mobintl.h into the keymap.deskintl.h, and modified the
    keymap.deskintl.h as follows to fix "<" (comma+shift), ">" (period+shift), "?"
    (slash+shift) keys.
    
    The following commit changed the key codes in the keymap.h for mobile. I
    reverted the change for the keymap.deskintl.h.
    https://github.com/jjsullivan5196/wvkbd/commit/9e4cf691377930e230c11ab4824165d86bf148cd
    
    Below is the difference between the keymap.mobintl.h and keymap.deskintl.h now.
    
    ```
    $ diff -u keymap.mobintl.h keymap.deskintl.h
    ...
    @@ -1226,9 +1225,9 @@
             key <AB05>               {	[               b,               B, equal ] };\
             key <AB06>               {	[               n,               N, question ] };\
             key <AB07>               {	[               m,               M, exclam ] };\
    -        key <AB08>               {	[           comma,      apostrophe, less, U00AB] };\
    -        key <AB09>               {	[          period,        question, greater, U00BB] };\
    -        key <AB10>               {	[           slash,        greater ] };\
    +        key <AB08>               {	[           comma,            less, backslash] };\
    +        key <AB09>               {	[          period,         greater, bar ] };\
    +        key <AB10>               {	[           slash,        question ] };\
             key <I147>               {  [      exclamdown,   questiondown, exclamdown ] };\
             key <RTSH>               {	[         Shift_R ] };\
             key <KPMU>               {\
    ...
    ```
    
    Co-authored-by: Jordan Johnston <johnstonljordan@gmail.com>
    Signed-off-by: Jun Aruga <jun.aruga@gmail.com>

---
+++ config.deskintl.h
@@ -0,0 +1,45 @@
+#ifndef config_h_INCLUDED
+#define config_h_INCLUDED
+
+#define DEFAULT_FONT "Sans 14"
+#define DEFAULT_ROUNDING 5
+static const int transparency = 255;
+
+struct clr_scheme schemes[] = {
+{
+  /* colors */
+  .bg = {.bgra = {15, 15, 15, transparency}},
+  .fg = {.bgra = {45, 45, 45, transparency}},
+  .high = {.bgra = {100, 100, 100, transparency}},
+  .swipe = {.bgra = {100, 255, 100, 64}},
+  .text = {.color = UINT32_MAX},
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* colors */
+  .bg = {.bgra = {15, 15, 15, transparency}},
+  .fg = {.bgra = {32, 32, 32, transparency}},
+  .high = {.bgra = {100, 100, 100, transparency}},
+  .swipe = {.bgra = {100, 255, 100, 64}},
+  .text = {.color = UINT32_MAX},
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+}
+};
+
+/* layers is an ordered list of layouts, used to cycle through */
+static enum layout_id layers[] = {
+  Full, // First layout is the default layout on startup
+  Special,
+  NumLayouts // signals the last item, may not be omitted
+};
+
+/* layers is an ordered list of layouts, used to cycle through */
+static enum layout_id landscape_layers[] = {
+  Full, // First layout is the default layout on startup
+  Special,
+  NumLayouts // signals the last item, may not be omitted
+};
+
+#endif // config_h_INCLUDED
